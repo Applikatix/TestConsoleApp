320d9653a4967cb59f6d9cffc7fdb060c3e822bc
jh <jh@PROELE77>
2023-08-10 12:31:22 +0200

    Add project files.

---
+++ TestClassLibrary/Name.cs
@@ -0,0 +1,36 @@
+﻿
+// Alle typer (klasser, structs, interfaces, etc.) er defineret i et namespace.
+// Namespaces hjælper med at organisere typer man definere.
+// Ting i et namespace kan tilgåes igennem det namespace, med mindre det har access modifieren "private".
+// namespace [name] { }
+namespace TestClassLibrary;
+
+// Klasse
+// [access modifier] class [name] { }
+public class Name {
+    // field
+    // "_input" med access modifieren "private" kan ikke tilgåes uden for klassen
+    // [access modifier] [type] [name];
+    private string _input;
+
+    // constructor
+    // [access modifier] [class name] ([parameters]) { [body] }
+    public Name(string input) {
+        // field instantiation
+        // [field name] = [argument];
+        _input = input;
+    }
+
+    // Method
+    // [access modifier] [return type] [name] ([parameters]) { [body] }
+    public int CountInput() {
+        // Return from method
+        // return [field name].[property];
+        return _input.Length;
+    }
+    // Static method
+    // [access modifier] static [return type] [name] ([parameters]) { [body] }
+    public static int CountString(string input) {
+        return input.Length;
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// Use
+// using [namespace];
+using TestClassLibrary;
+
+// Method call
+// [class].[method]([argument]);
+Console.WriteLine("Indtast noget:");
+// Instansiation
+// [class] [variabel] = new [constructor]([argument]);
+Name nameClass = new Name(Console.ReadLine());
+
+// [type] [variabel] = [object].[method]();
+int len = nameClass.CountInput();
+Console.WriteLine($"Længde på indtastning: {len}");
+    
